518235e3f89de9f81d875f961cae68476b9c04fa
Ehsan Barghamadi <ehsan0barghamadi@gmail.com>
2026-02-17 16:56:32 +0330

    feat: implement environment variables for database credentials

--- app/utils/db_handler.py
+++ app/utils/db_handler.py
@@ -1,16 +1,27 @@
+from dotenv import load_dotenv
 import psycopg2
+import os
+load_dotenv()
 
 def get_connection():
     '''
     This function is for connecting to the database.
     '''
+    value = [os.getenv("DB_NAME"),
+            os.getenv("DB_USERNAME"),
+            os.getenv("DB_PASSWORD"),
+            os.getenv("DB_HOST"),
+            os.getenv("DB_PORT")]
+    if None in value:
+        print("Please define the variables in the env file correctly.(referral .env.example)")
+        return None
     try:
         connection = psycopg2.connect(
-            dbname="restaurant_db",
-            user="postgres",
-            password="pass",
-            host="localhost",
-            port="5432"
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USERNAME"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT")
         )
         return connection
     
